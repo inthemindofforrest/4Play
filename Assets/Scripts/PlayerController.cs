70477a80a570798395de4556ba25ba814d7004c3
Ryan Healy <ryan.healy11@gmail.com>
2020-01-23 10:58:32 -0800

    look left everyone

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,50 +8,87 @@ public class PlayerController : MonoBehaviour
     private Vector3 posChange = new Vector3(0,0,0);
     public Transform Camera;
     public float floatHight;
-    
+    public bool left = false;
+    private Vector3 flip = new Vector3(0, 180, 0);
     void Start()
-    {
-        Debug.Log(Camera.right);
-        
+    {          
     }
 
     
     void Update()
-    {
-        
-       
-
+    {            
+        // set the players hight above the current floor and canges the players rotation based on the cameras rotation
         RaycastHit hit;
         if (Physics.Raycast(transform.position, -transform.up, out hit, Mathf.Infinity))
         {
             transform.position = hit.point + new Vector3(0, floatHight, 0);
 
             Vector3 playerRotation = transform.rotation.eulerAngles;
-            Vector3 camerRotation = Camera.rotation.eulerAngles;
-            Vector3 floorRotation = hit.transform.rotation.eulerAngles;
+            Vector3 camerRotation = Camera.rotation.eulerAngles;           
             Quaternion newRot = new Quaternion();
-            newRot.eulerAngles = new Vector3(playerRotation.x, camerRotation.y, playerRotation.z);
-            transform.rotation = newRot;
-            ////transform.rotation.eulerAngles = new Vector3(floorRotation.x, camerRotation.y, floorRotation.z);
-
-            //transform.up = new Vector3( hit.transform.up.x, camerRotation.y,hit.transform.up.z);
+            if (left)
+            {
+                newRot.eulerAngles = new Vector3(playerRotation.x, camerRotation.y + flip.y, playerRotation.z);
+                transform.rotation = newRot;
+            }
+            else
+            {
+                newRot.eulerAngles = new Vector3(playerRotation.x, camerRotation.y, playerRotation.z);
+                transform.rotation = newRot;
+            }            
         }
 
+        //moves the player based off of wasd
         if (Input.GetKey(KeyCode.A))
         {
-            posChange += (/*-Camera.right*/ -transform.right);
+            if (!left)
+            {
+                transform.Rotate(flip);
+                left = true;
+                posChange += (transform.right);
+            }
+            else
+            {
+                posChange += (transform.right);
+            }   
         }
+
         if (Input.GetKey(KeyCode.D))
         {
-            posChange += ( /*Camera.right*/transform.right);
+            if (left)
+            {
+                transform.Rotate(flip);
+                left = false;
+                posChange += (transform.right);
+            }
+            else
+            {
+                posChange += (transform.right);
+            }
         }
+
         if (Input.GetKey(KeyCode.W))
         {
-            posChange += (/*Camera.forward*/transform.forward);
+            if (left)
+            {
+                posChange += (-transform.forward);
+            }
+            else
+            {
+                posChange += (transform.forward);
+            }
         }
+
         if (Input.GetKey(KeyCode.S))
         {
-            posChange += (/*-Camera.forward*/ -transform.forward);
+            if (left)
+            {
+                posChange += (transform.forward);
+            }
+            else
+            {
+                posChange += (-transform.forward);
+            }
         }
         
         transform.position += (posChange.normalized * Speed * Time.deltaTime);
